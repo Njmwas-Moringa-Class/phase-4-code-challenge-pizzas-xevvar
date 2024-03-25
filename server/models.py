6ac66523396b8680badae494a5cf9d2c782f4209
Anthony <gachinigaithuma@gmail.com>
2024-03-25 15:44:57 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import relationship, validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -18,9 +18,22 @@ class Restaurant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     address = db.Column(db.String)
 
-    # add relationship
+    restaurant_pizzas = relationship(
+        'RestaurantPizza', back_populates='restaurant', cascade='all, delete-orphan')
 
-    # add serialization rules
+    def to_dict(self, include_pizzas=False):
+        if include_pizzas:
+            return {
+                "id": self.id,
+                "name": self.name,
+                "address": self.address,
+                "restaurant_pizzas": [rp.to_dict() for rp in self.restaurant_pizzas]
+            }
+        return {
+            "id": self.id,
+            "name": self.name,
+            "address": self.address,
+        }
 
     def __repr__(self):
         return f'<Restaurant {self.name}>'
@@ -33,9 +46,15 @@ class Pizza(db.Model, SerializerMixin):
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
 
-    # add relationship
+    restaurant_pizzas = relationship(
+        'RestaurantPizza', back_populates='pizza', cascade='all, delete-orphan')
 
-    # add serialization rules
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "ingredients": self.ingredients,
+        }
 
     def __repr__(self):
         return f'<Pizza {self.name}, {self.ingredients}>'
@@ -46,12 +65,27 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, ForeignKey('restaurants.id'))
+    pizza_id = db.Column(db.Integer, ForeignKey('pizzas.id'))
 
-    # add relationships
+    restaurant = relationship('Restaurant', back_populates='restaurant_pizzas')
+    pizza = relationship('Pizza', back_populates='restaurant_pizzas')
 
-    # add serialization rules
+    @validates('price')
+    def validate_price(self, key, price):
+        if not (1 <= price <= 30):
+            raise ValueError("Price must be between 1 and 30.")
+        return price
 
-    # add validation
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "price": self.price,
+            "pizza": self.pizza.to_dict(),
+            "pizza_id": self.pizza_id,
+            "restaurant": self.restaurant.to_dict(),
+            "restaurant_id": self.restaurant_id
+        }
 
     def __repr__(self):
         return f'<RestaurantPizza ${self.price}>'
--- server/seed.py
+++ server/seed.py
@@ -16,7 +16,7 @@ with app.app_context():
     shack = Restaurant(name="Karen's Pizza Shack", address='address1')
     bistro = Restaurant(name="Sanjay's Pizza", address='address2')
     palace = Restaurant(name="Kiki's Pizza", address='address3')
-    restaurants = [shack, bistro, palace]
+    db.session.add_all([shack, bistro, palace])
 
     print("Creating pizzas...")
 
@@ -25,17 +25,14 @@ with app.app_context():
         name="Geri", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
     california = Pizza(
         name="Melanie", ingredients="Dough, Sauce, Ricotta, Red peppers, Mustard")
-    pizzas = [cheese, pepperoni, california]
+    db.session.add_all([cheese, pepperoni, california])
 
     print("Creating RestaurantPizza...")
 
     pr1 = RestaurantPizza(restaurant=shack, pizza=cheese, price=1)
     pr2 = RestaurantPizza(restaurant=bistro, pizza=pepperoni, price=4)
     pr3 = RestaurantPizza(restaurant=palace, pizza=california, price=5)
-    restaurantPizzas = [pr1, pr2, pr3]
-    db.session.add_all(restaurants)
-    db.session.add_all(pizzas)
-    db.session.add_all(restaurantPizzas)
-    db.session.commit()
+    db.session.add_all([pr1, pr2, pr3])
 
+    db.session.commit()
     print("Seeding done!")
